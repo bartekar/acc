a33c65ca1004c7ba3818242ca9eb820b5bb9e496
kbartecki <55534106+bartekar@users.noreply.github.com>
2022-05-10 01:49:55 +0200

    [ADD] KPIs

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ dist0= 250
 # simulation length [s]
 T= 60
 # simulation step [s], screw around with this parameter too much and you an invalid simulation
-dt= 0.2
+dt= 0.05
 
 # automatic generated variables
 
@@ -63,6 +63,10 @@ X[3,0]= v1
 u= np.zeros((2,sim_length))
 u[0,:]= v0*r # acceleration for leading. This value is set such that the velocity does not change (terminal velocity)
 u[1,:]= 4.5 # acceleration for follower
+
+u[1,:600]= 4.5
+u[1,600:700]= np.linspace(4.5, 3.0, 100)
+u[1,700:]= 3.0
 
 # %% 3) simulation
 
@@ -100,12 +104,70 @@ plt.ylim([0,50])
 plt.ylabel('velocity [m/s]')
 plt.xlabel('simulation time [s]')
 
+# %% 5) kpis
+
+def calc_kpi_crash(s_leader, s_follower):
+    kpi= np.zeros_like(s_leader)
+    dist_prev= s_leader[0] - s_follower[0]
+    for k in range(1, s_leader.shape[0]):
+        dist_now= s_leader[k] - s_follower[k]
+        if np.sign(dist_now) != np.sign(dist_prev):
+            kpi[k]= 1
+        dist_prev= dist_now
+    return kpi
+
+# plt.plot(calc_kpi_crash(X[0,:], X[2,:]))
 
 
+#   A kpi
+#   |
+# 1 *
+#   | *
+#   |   *                       *
+#   |     *                 *
+#   |       *           *
+# 0 +---------*--*--*--------- > distance between vehicles
+#   0         d_s   d_i
+# 
+# safety distance is defined as d_s = 2*v
+# ignorance window is the range from d_S to d_i= (1+i_win)*d_s
+# from i on there is another penalty, that increases with higher distance
+# 
+def calc_kpi_distance(s_leader, s_follower, v_follower, ignorance_win=0.3, penalty_slope=0.2):
+    kpi= np.zeros_like(s_leader)
+    dist_vehicles= s_leader - s_follower
+    for k in range(0, s_leader.shape[0]):
+        safety_dist= 2*v_follower[k]
+        ignore_dist= (1+ignorance_win) * safety_dist
+        
+        if dist_vehicles[k] > ignore_dist:    # right of the diagram
+            excess_dist= dist_vehicles[k] - ignore_dist
+            kpi[k]= excess_dist/safety_dist *penalty_slope
+        elif dist_vehicles[k] > safety_dist:  # inside the window of ignorance
+            kpi[k]= 0.0
+        elif dist_vehicles[k] >= 0.0:         # diving into safety distance
+            kpi[k]= 1.0 - dist_vehicles[k]/safety_dist
+        elif dist_vehicles[k] > -3.0:         # inside the other vehicle...
+            kpi[k]= 1.0
+        elif dist_vehicles[k] <= -3.0:        # "follower" is in front of the "leading vehicle" -> this is fine
+            kpi[k]= 0.0
+        else:
+            raise Exception('this should not have happened')
+    return kpi
+
+# plt.plot(calc_kpi_distance(X[0,:], X[2,:], X[3,:]))
+
+# unfinished... is it necessary?
+def calc_kpi_jerk(a, dt, j_dangerous):
+    j= np.zeros_like(a)
+    j[1:]= (a[1:]-a[:-1]) /dt
+    # j/= a_max
+    return j
+
+# plt.plot(calc_kpi_jerk(u[1,:], dt, j_dangerous=4.5))
 
 
 # todos:
-    # kpi
     # self tuned PID controller for single v0
     # implement control through state
     # evolutionary algorithms
